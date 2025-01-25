3c462400d7cf8cd6370f80597df08fb5cfd9a707
Ardiona <140248174+ArdionaRahadi@users.noreply.github.com>
2025-01-25 17:49:24 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let register = document.querySelector("button.register");
 let login = document.querySelector("button.login");
 let formSide = document.querySelector(".form-side");
@@ -44,40 +43,3 @@ login.addEventListener("click", () => {
       "slideRightToLeft 1s ease-in-out forwards";
   }
 });
-=======
-const btnRegister = document.querySelector('.btn-register');
-const btnLogin = document.querySelector('.btn-login');
-const sideForm = document.querySelector('.side-form');
-const formSignUp = document.querySelector('.form-content.register');
-const formLogin = document.querySelector('.form-content.login');
-const registerText = document.querySelector('.register-text');
-const loginText = document.querySelector('.login-text');
-
-btnRegister.addEventListener('click', () => {
-  if (window.innerWidth <= 750) {
-    formLogin.classList.add('active');
-    formSignUp.classList.add('active');
-    registerText.classList.add('active');
-    loginText.classList.add('active');
-  } else {
-    sideForm.style.animation = "registerActive 1.4s ease-in-out forwards";
-    registerText.style.animation = "registerSlideOut 1s ease-in-out forwards";
-    loginText.style.animation = "registerSlideIn 1.5s ease-in-out forwards";
-  }
-  
-})
-
-btnLogin.addEventListener('click', () => {
-  if (window.innerWidth <= 750) {
-    formLogin.classList.remove('active');
-    formSignUp.classList.remove('active');
-    registerText.classList.remove('active');
-    loginText.classList.remove('active');
-  } else {
-    sideForm.style.animation = "loginActive 1.4s ease-in-out forwards";
-    loginText.style.animation = "loginSlideOut 1s ease-in-out forwards";
-    registerText.style.animation = "loginSlideIn 1.5s ease-in-out forwards";
-  }
-  
-})
->>>>>>> 005a3d2da2b92f22803bd7ce7066109a38d93590
